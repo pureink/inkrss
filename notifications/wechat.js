a4e1fd5df070358bd1184b214a6f5f375818a0fc
pureink <pureink@tutanota.com>
2021-07-30 10:32:50 +0800

    Update wechat.js

--- notifications/wechat.js
+++ notifications/wechat.js
@@ -1,10 +1,11 @@
-import { config } from '../config'
+import { config } from "../config";
 export async function reply(feed, item) {
-    await fetch(config.WECHAT_URL + `${feed.title}\n${item.title}`)
+  await fetch(
+    `${config.WECHAT_URL}&msg_type=text&msg=${feed.title}\n${item.title}\n${item.link}`
+  );
 }
 export async function replyWhenError(feed) {
-    await fetch(
-        config.WECHAT_URL +
-            `${feed.title}\n连续多次失败，将暂停更新，请检查订阅源是否正常。`
-    )
+  await fetch(
+    `${config.WECHAT_URL}&msg_type=text&msg=${feed.title}\n连续多次失败，将暂停更新，请检查订阅源是否正常。`
+  );
 }
