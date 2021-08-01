40a4535f9bd3e350d3720d2c32df77ef43c9e9c3
pureink <pureink@tutanota.com>
2021-08-01 19:12:22 +0800

    solve markdown parse problem

--- notifications/telegram.js
+++ notifications/telegram.js
@@ -1,26 +1,27 @@
-import { config } from '../config'
-const Telegram = require('telegraf/telegram')
-import { telegraph } from '../utils/telegraph'
-let sendto = config.TG_USERID
+import { config } from "../config";
+const Telegram = require("telegraf/telegram");
+import { telegraph } from "../utils/telegraph";
+import { md } from "../utils/markdown";
+let sendto = config.TG_USERID;
 export async function reply(feed, item) {
-    const telegram = new Telegram(config.TG_TOKEN)
-    await telegram.sendMessage(
-        sendto,
-        `*${feed.title}*\n${item.title}\n${
-            feed.telegraph
-                ? item.content
-                    ? `[telegraph](${await telegraph(item)})`
-                    : ''
-                : ''
-        }  ${item.link ? `[查看原文](${item.link})` : ''}`,
-        { parse_mode: 'MARKDOWN' }
-    )
+  const telegram = new Telegram(config.TG_TOKEN);
+  await telegram.sendMessage(
+    sendto,
+    `*${md(feed.title)}*\n${md(item.title)}\n${
+      feed.telegraph
+        ? item.content
+          ? `[telegraph](${await telegraph(item)})`
+          : ""
+        : ""
+    }  ${item.link ? `[查看原文](${item.link})` : ""}`,
+    { parse_mode: "MARKDOWN" }
+  );
 }
 export async function replyWhenError(feed) {
-    const telegram = new Telegram(config.TG_TOKEN)
-    await telegram.sendMessage(
-        sendto,
-        `*${feed.title}*\n 连续多次失败，将暂停更新，请检查订阅源是否正常。`,
-        { parse_mode: 'MARKDOWN' }
-    )
+  const telegram = new Telegram(config.TG_TOKEN);
+  await telegram.sendMessage(
+    sendto,
+    `*${md(feed.title)}*\n 连续多次失败，将暂停更新，请检查订阅源是否正常。`,
+    { parse_mode: "MARKDOWN" }
+  );
 }
--- utils/tgbot_command/list.js
+++ utils/tgbot_command/list.js
@@ -1,13 +1,14 @@
+import { md } from "../markdown";
 export async function botList(ctx) {
-    let subraw = (await KV.get('sub')) || '[]'
-    const sub = JSON.parse(subraw)
-    if (sub.length == 0) {
-        await ctx.reply('还没有进行过订阅')
-    } else {
-        let msg = ''
-        for (let i = 0; i < sub.length; i++) {
-            msg += `[${sub[i].title}](${sub[i].url})\n`
-        }
-        await ctx.reply(msg,{parse_mode: 'Markdown'})
+  let subraw = (await KV.get("sub")) || "[]";
+  const sub = JSON.parse(subraw);
+  if (sub.length == 0) {
+    await ctx.reply("还没有进行过订阅");
+  } else {
+    let msg = "";
+    for (let i = 0; i < sub.length; i++) {
+      msg += `[${md(sub[i].title)}](${sub[i].url})\n`;
     }
+    await ctx.reply(msg, { parse_mode: "Markdown" });
+  }
 }
